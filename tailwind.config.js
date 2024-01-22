0727ef1f5e869fb87e345874ce8b9230d027bcd2
iamcharlie <riyadhosen40@gmail.com>
2024-01-22 22:35:48 +0600

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'red-color': '#FF4240',
+      },
+    },
+  },
+};
